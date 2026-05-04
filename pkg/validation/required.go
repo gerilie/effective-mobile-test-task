661621c023d9e709966c35a8e823d2e6fe807794
Michael <gerilie@yandex.ru>
2026-05-04 18:13:19 +0300

    refactor(validation): correct error message for required tag

--- pkg/validation/required.go
+++ pkg/validation/required.go
@@ -9,9 +9,12 @@ import (
 func getErrorMessageForRequiredTag(fe validator.FieldError) string {
 	switch fe.Tag() {
 	case "required":
-		return "The field is required"
+		return "The field must be present"
 	case "required_with":
-		return fmt.Sprintf("The field is required when '%s' is present", fe.Param())
+		return fmt.Sprintf(
+			"The field must be present when '%s' is present",
+			fe.Param(),
+		)
 	}
 
 	return ""
