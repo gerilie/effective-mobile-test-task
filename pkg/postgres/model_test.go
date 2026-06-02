32a503c9ba5f382e1e80e5b7e0a7a48ac459e910
Michael <gerilie@yandex.ru>
2026-06-02 13:17:17 +0300

    test(postgres): separate test cases

--- pkg/postgres/model_test.go
+++ pkg/postgres/model_test.go
@@ -11,7 +11,7 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
-func Test_New(t *testing.T) {
+func Test_New_Success(t *testing.T) {
 	t.Parallel()
 
 	ctrl := gomock.NewController(t)
@@ -21,57 +21,67 @@ func Test_New(t *testing.T) {
 	ctx := logger.WithLogger(context.Background(), mockLogger)
 	cfg := postgres.Config{}
 
-	t.Run("success", func(t *testing.T) {
-		t.Parallel()
+	mockLogger.EXPECT().Info("connected to database")
 
-		mockLogger.EXPECT().Info("connected to database")
-
-		gomock.InOrder(
-			mockPoolConnector.EXPECT().
-				Connect(gomock.Any(), gomock.Any()).
-				Return(&pgxpool.Pool{}, nil),
-			mockPoolConnector.EXPECT().Ping(gomock.Any(), gomock.Any()).Return(nil),
-		)
-
-		pool, err := postgres.New(ctx, cfg, mockPoolConnector)
-
-		require.NoError(t, err)
-		require.NotNil(t, pool)
-	})
-
-	t.Run("connection failed", func(t *testing.T) {
-		t.Parallel()
-
-		mockLogger.EXPECT().Info(gomock.Any()).Times(0)
+	gomock.InOrder(
 		mockPoolConnector.EXPECT().
 			Connect(gomock.Any(), gomock.Any()).
-			Return(nil, postgres.ErrConnectionFailed)
+			Return(&pgxpool.Pool{}, nil),
+		mockPoolConnector.EXPECT().Ping(gomock.Any(), gomock.Any()).Return(nil),
+	)
 
-		pool, err := postgres.New(ctx, cfg, mockPoolConnector)
+	pool, err := postgres.New(ctx, cfg, mockPoolConnector)
 
-		require.Nil(t, pool)
-		require.Error(t, err)
-		require.ErrorIs(t, err, postgres.ErrConnectionFailed)
-	})
+	require.NoError(t, err)
+	require.NotNil(t, pool)
+}
 
-	t.Run("ping failed", func(t *testing.T) {
-		t.Parallel()
+func Test_New_ConnectionFailed(t *testing.T) {
+	t.Parallel()
 
-		mockLogger.EXPECT().Info(gomock.Any()).Times(0)
-		gomock.InOrder(
-			mockPoolConnector.EXPECT().
-				Connect(gomock.Any(), gomock.Any()).
-				Return(&pgxpool.Pool{}, nil),
-			mockPoolConnector.EXPECT().
-				Ping(gomock.Any(), gomock.Any()).
-				Return(postgres.ErrConnectionFailed),
-			mockPoolConnector.EXPECT().Close(&pgxpool.Pool{}),
-		)
+	ctrl := gomock.NewController(t)
+	mockLogger := logger.NewMockLogger(ctrl)
+	mockPoolConnector := postgres.NewMockPoolConnector(ctrl)
 
-		pool, err := postgres.New(ctx, cfg, mockPoolConnector)
+	ctx := logger.WithLogger(context.Background(), mockLogger)
+	cfg := postgres.Config{}
 
-		require.Nil(t, pool)
-		require.Error(t, err)
-		require.ErrorIs(t, err, postgres.ErrConnectionFailed)
-	})
+	mockLogger.EXPECT().Info(gomock.Any()).Times(0)
+	mockPoolConnector.EXPECT().
+		Connect(gomock.Any(), gomock.Any()).
+		Return(nil, postgres.ErrConnectionFailed)
+
+	pool, err := postgres.New(ctx, cfg, mockPoolConnector)
+
+	require.Nil(t, pool)
+	require.Error(t, err)
+	require.ErrorIs(t, err, postgres.ErrConnectionFailed)
+}
+
+func Test_New_PingFailed(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	mockLogger := logger.NewMockLogger(ctrl)
+	mockPoolConnector := postgres.NewMockPoolConnector(ctrl)
+
+	ctx := logger.WithLogger(context.Background(), mockLogger)
+	cfg := postgres.Config{}
+
+	mockLogger.EXPECT().Info(gomock.Any()).Times(0)
+	gomock.InOrder(
+		mockPoolConnector.EXPECT().
+			Connect(gomock.Any(), gomock.Any()).
+			Return(&pgxpool.Pool{}, nil),
+		mockPoolConnector.EXPECT().
+			Ping(gomock.Any(), gomock.Any()).
+			Return(postgres.ErrConnectionFailed),
+		mockPoolConnector.EXPECT().Close(&pgxpool.Pool{}),
+	)
+
+	pool, err := postgres.New(ctx, cfg, mockPoolConnector)
+
+	require.Nil(t, pool)
+	require.Error(t, err)
+	require.ErrorIs(t, err, postgres.ErrConnectionFailed)
 }
